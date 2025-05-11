bbd861c5d67eaf580b41b831bf7a22c0654d8efd
Matías <matias.diaz.reyes2005@gmail.com>
2025-05-11 01:52:02 -0400

    Mi primer commit

---
+++ src/test/java/com/example/Bibliotecaduoc/BibliotecaduocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bibliotecaduoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaduocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
